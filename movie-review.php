c7ba7731ef192e5a96de5902208cb33a1c65706c
Vinicius Pereira de Oliveira <agavi2014@hotmail.com>
2020-08-20 21:50:16 -0300

    first commit

---
+++ movie-review.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Plugin Name: Movie Review
+Plugin URI:
+Description: Criação de um plugin para administração de um cinema
+Version: 1.0
+Author: Vinícius Pereira de Oliveira
+Author URI:
+Text Domain:
+License:
+*/
